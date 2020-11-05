1cd5ea0959b505a8f33b8b42d7fbefb14d9e1c2e
anthonymjimenez <anthonyjim96@gmail.com>
2020-11-04 22:54:40 -0500

    submit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,109 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
+  const dogTable = document.getElementById("dog-table");
+  const dogName = document.getElementById("dog-name");
+  const dogSex = document.getElementById("dog-sex");
+  const dogBreed = document.getElementById("dog-breed");
+  const dogForm = document.getElementById("dog-form");
+  const URL = `http:localhost:3000/dogs`;
+  var GLOBALID;
 
-})
+  (() => {
+    dogSex.value = "";
+    dogBreed.value = "";
+    dogName.value = "";
+  })();
+
+  const createNode = (elmType) => (str) => {
+    const element = document.createElement(elmType);
+    element.innerText = str;
+    return element;
+  };
+
+  const getId = (id) => () => id;
+
+  const createTd = createNode("td");
+  const createBtn = createNode("button");
+
+  const editText = (domObj, str) => (domObj.value = str);
+
+  const editDog = async (id) => {
+    const dogElement = await getDog(id);
+    editText(dogName, dogElement.name);
+    editText(dogBreed, dogElement.breed);
+    editText(dogSex, dogElement.sex);
+    GLOBALID = getId(id);
+  };
+
+  async function getDog(id) {
+    let response = await fetch(`${URL}/${id}`);
+
+    let dog = await response.json();
+
+    return dog;
+  }
+
+  async function getDogs() {
+    let response = await fetch(URL);
+
+    let dogs = await response.json();
+
+    return dogs;
+  }
+
+  addEventToForm();
+  renderDogs();
+
+  async function renderDogs() {
+    let dogs = await getDogs();
+
+    dogs.forEach((dog) => {
+      const tr = document.createElement("tr");
+      const editButton = createBtn("edit");
+
+      editButton.addEventListener("click", () => editDog(dog.id));
+      const name = createTd(dog.name);
+      const breed = createTd(dog.breed);
+      const sex = createTd(dog.sex);
+
+      tr.append(name, breed, sex, editButton);
+      tr.id = `A${dog.id}`;
+      dogTable.append(tr);
+    });
+  }
+
+  async function patchDog() {
+    configObj = {
+      method: "PATCH",
+      headers: {
+        "Content-type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        name: dogName.value,
+        breed: dogBreed.value,
+        sex: dogSex.value,
+      }),
+    };
+    console.log(`${URL}/${GLOBALID()}`);
+
+    let dogres = await fetch(`${URL}/${GLOBALID()}`, configObj);
+    let dog = await dogres.json();
+    const dog_table = document.querySelector(`#A${dog.id}`);
+    const [name, breed, sex] = dog_table.querySelectorAll("td");
+    console.log(name);
+    name.innerText = dog.name;
+    breed.innerText = dog.breed;
+    sex.innerText = dog.sex;
+    // console.log(update)
+  }
+
+  function addEventToForm() {
+    dogForm.addEventListener("submit", (e) => {
+      e.preventDefault();
+      console.log("FUCK Ou");
+      patchDog();
+    });
+  }
+
+  renderDogs();
+});
